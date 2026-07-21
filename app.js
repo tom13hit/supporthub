540154820f75ad07318e9d73bb32cbbccfe98168
Thom <thomas1307vicenzo@gmail.com>
2026-07-20 21:17:22 -0300

    primeira versão do supporthub

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
+app.get("/login", (req, res) => {
+    res.render("auth/login");
+});
+
+app.get("/cadastro", (req, res) => {
+    res.render("auth/cadastro");
+});
+
+app.get("/dashboard", (req, res) => {
+    res.render("cliente/dashboard");
+});
+
+app.get("/admin", (req, res) => {
+    res.render("admin/dashboard");
+});
